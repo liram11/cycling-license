032cf63e791f84fdfa5926a28d7a14c6167a2d9c
liram <liram07@gmail.com>
2022-05-29 22:33:43 +0300

    rabbit task publisher added

---
+++ app/services/rabbit/task_publisher.rb
@@ -0,0 +1,18 @@
+module Rabbit
+  class TaskPublisher < ApplicationService
+    def initialize(queue_name:, data:)
+      raise ArgumentError, 'queue_name should be a String' unless queue_name.is_a?(String)
+      raise ArgumentError, 'data should be a Hash' unless data.is_a?(Hash)
+
+      @queue_name = queue_name
+      @message = data.to_json
+    end
+
+    def call
+      channel = Rabbitmq.connect
+      queue = channel.queue(@queue_name, durable: true)
+
+      queue.publish(@message, persistent: true)
+    end
+  end
+end
